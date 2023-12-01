22c7b997ae85bd35329390f476572c25c87d0812
Gitros <31899790+Gitros@users.noreply.github.com>
2023-12-01 14:37:56 +0100

    passing custom arguments to event functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  function handleClick(clickedButton) {
+
+    console.log(clickedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +25,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton onClick={() => handleClick('state')}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
